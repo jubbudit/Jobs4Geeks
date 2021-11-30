53fb3522c5a7fed41a5dc04f94d3e26353d6404a
JonathanH1998 <coltraxjon@gmail.com>
2021-11-30 14:49:46 -0500

    updating the about me page for Jonathan

--- src/main/java/edu/vt/globals/Constants.java
+++ src/main/java/edu/vt/globals/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
     //-------------------------
     public static final String USER_PHOTOS_ABSOLUTE_PATH = "C:/Users/conne/DocRoot/Jobs4GeeksStorage/CandidatePhotoStorage/";
     public static final String USER_PHOTOS_URI = "http://localhost:8080/candidatephotos/";
-    public static final String USER_FILES_ABSOLUTE_PATH = "C:/Users/conne/DocRoot/Jobs4GeeksStorage/CandidatePhotoStorage/";
+    public static final String USER_FILES_ABSOLUTE_PATH = "C:/Users/conne/DocRoot/Jobs4GeeksStorage/CandidateFilesStorage/";
     public static final String USER_FILES_URI = "http://localhost:8080/candidatefiles/";
     public static final String COMPANY_PHOTOS_ABSOLUTE_PATH = "C:/Users/conne/DocRoot/Jobs4GeeksStorage/CompanyPhotoStorage/";
     public static final String COMPANY_PHOTOS_URI = "http://localhost:8080/companyphotos/";
